8f7fc550b366de7900efff6833f78a5014b4d2de
Xing Li <msuli_1120@ymail.com>
2017-04-21 14:54:17 -0700

    add specialties

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -3,8 +3,9 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 var displayData = function(medissue, result){
   $('#divtitle').text('Doctors that can treat ' + medissue + ' issues are:');
   result.data.forEach(function(obj){
-    $('#output').append('<li><div class="well" id="doctor"><h2>Name: ' + obj.profile.first_name + ' ' + obj.profile.last_name + '</h2><br><span class="head">Gender</span>: ' + obj.profile.gender + '<br><img src="' + obj.profile.image_url + '"><br><span class="head">Languages</span>: ' + obj.profile.languages[0].name + '<br><span class="head">Bio</span>: ' + obj.profile.bio  + '</li></ol>');
+    $('#output').append('<div class="row"><div class="well col-md-7"><h2>Name: ' + obj.profile.first_name + ' ' + obj.profile.last_name + '</h2><br><span class="head">Gender</span>: ' + obj.profile.gender + '<br><img src="' + obj.profile.image_url + '"><br><span class="head">Languages</span>: ' + obj.profile.languages[0].name + '<br><span class="head">Bio</span>: ' + obj.profile.bio + '</div><div class="col-md-4 well"><span class="head">Specialties</span><br><li>Actor: ' + obj.specialties[0].actor + '</li><br><li>Description: ' + obj.specialties[0].description +'</li></div></div>');
   });
+
 };
 
 $(function(){
@@ -13,6 +14,7 @@ $(function(){
   $('form').submit(function(event){
     event.preventDefault();
     $('#output').empty();
+    $('#specialties').empty();
     var medissue = $('input#issue').val();
     doctor.getDoctor(medissue, displayData);
   });
